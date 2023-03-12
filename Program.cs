b8a36a2a93a2473eaf05431f0c864c7b052adc10
KabakovaV <126878904+KabakovaV@users.noreply.github.com>
2023-03-12 13:11:41 +0700

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Lesson1;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("void your name: ");
+        var name = Console.ReadLine();
+        Console.WriteLine($"Привет, {name}");
+    }
+}
+
